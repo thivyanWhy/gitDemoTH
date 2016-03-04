c17ea272d65c5f67f8e25637adf0c13efc72a791
thivyanWhy <thivyan6@googlemail.com>
2016-03-04 16:14:07 +0530

    Changed background colour and added a constant called userName to the View Controller file.

--- GitDemoTH/ViewController.swift
+++ GitDemoTH/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let userName = "DummyUser"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
